6d91a291577006a1a2371e3c97d868fa173d423f
Samuel Pokam <dixiades@gmail.com>
2023-05-12 12:23:41 +0100

    Google Auth updates

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 
 const Nav = () => {
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
 
   const [providers, setProviders] = useState(null)
   const [toggleDropdown, setToggleDropdown] = useState(false)
@@ -35,7 +35,7 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="create-prompt" className="black_btn">
               Create Post
@@ -49,7 +49,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 alt="profile"
                 width={37}
                 height={37}
@@ -76,14 +76,14 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               alt="profile"
               width={37}
               height={37}
-              className="rounded-full"
+              className="rounded-full cursor-pointer"
               onClick={() => setToggleDropdown((prev) => !prev)}
             />
 
